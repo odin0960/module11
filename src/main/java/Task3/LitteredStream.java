2005105c783892e9a8652f2445b6e74d09b0ef8d
odin0960 <121524275+odin0960@users.noreply.github.com>
2023-04-21 16:12:48 +0300

    Task 3 "1, 2, 0", "4, 5"

--- src/main/java/Task3/LitteredStream.java
+++ src/main/java/Task3/LitteredStream.java
@@ -1,5 +1,6 @@
 package Task3;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -8,18 +9,27 @@ public class LitteredStream {
 
     public static void main(String[] args) {
 
+        //Варіант 1
         String[] numbers = {"\"1", "2", "\"8", "0\"", "\"4", "5\""};
         System.out.println(Arrays.toString(numbers));
-        Object[] numbersObj = {"\"1", "2", "\"8", "0\"", "\"4", "5\""};
 
-        // Швидкий вивід рядка "чистих" чисел як стрінгів
+        //Варіант 2
+        Object[] numbersObj = {"\"1", "2", "\"8", "0\"", "\"4", "5\""};
+        System.out.println(Arrays.toString(numbersObj));
+
+        //Варіант 3
+        String[] numbersOther = {"1, 2, 0", "4, 5"};
+        System.out.println(Arrays.toString(numbersOther));
+
+
+        //Варіант 1
         System.out.println(Arrays.stream(numbers)
                 .map(n -> n.replace("\"", ""))
                 .sorted()
                 .collect(Collectors.joining(", ")));
 
 
-        // Або створення очищеного списку з приведенням типів до Integr
+        //Варіант 2
         List<Integer> numberList = Arrays.stream(numbersObj)
                 .map(n -> n + "")
                 .map(n -> n.replace("\"", ""))
@@ -27,9 +37,21 @@ public class LitteredStream {
                 .sorted()
                 .toList();
 
-        // і роздруківка списку
         System.out.println(numberList.stream()
                 .map(String::valueOf)
                 .collect(Collectors.joining(", ")));
+
+
+        //Варіант 3
+        List<Integer> numberListOther = Arrays.stream(numbersOther)
+                .flatMap(n -> Arrays.stream(n.split("\\s*,\\s*")))
+                .map(Integer::parseInt)
+                .sorted()
+                .toList();
+
+        System.out.println(numberListOther.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(", ")));
+
     }
 }
